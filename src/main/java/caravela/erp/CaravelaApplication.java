f0517cba5a75d3f950176b9cd0d214ee6f88faa7
Thiago Alessandro <114539167+ThiagoIsland@users.noreply.github.com>
2024-07-16 21:27:03 -0300

    First commit

---
+++ src/main/java/caravela/erp/CaravelaApplication.java
@@ -0,0 +1,13 @@
+package caravela.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaravelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaravelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/caravela/erp/CaravelaApplicationTests.java
@@ -0,0 +1,13 @@
+package caravela.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
